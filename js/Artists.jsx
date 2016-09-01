68299a423f53807ad5d4eeb2a9ae5710f3c04d72
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-09-01 12:28:08 -1000

    made a search bar on the landing page ;

--- js/Artists.jsx
+++ js/Artists.jsx
@@ -2,14 +2,34 @@ const React = require('react')
 const data = require('../public/data')
 const { Link } = require('react-router')
 
-const Artists = () => (
-  <div>
-    {data.artists.map((artist) => (
-      <Link to={`/artist/${artist.name}`}>
-        <h4 className=''>{artist.name}</h4>
-      </Link>
-    ))}
-  </div>
-)
+const Artists = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
+  render () {
+    return (
+      <div>
+        <h2>{this.state.searchTerm}</h2>
+        <input value={this.state.searchTerm} type='text' placeholder='Search' onChange={this.handleSearchTermEvent}></input>
+
+        <div>
+          {data.artists
+            .filter((artist) => `${artist.name}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((artist) => (
+              <Link to={`/artist/${artist.name}`}>
+                <h4 className=''>{artist.name}</h4>
+              </Link>
+          ))}
+        </div>
+
+      </div>
+    )
+  }
+})
 
 module.exports = Artists
