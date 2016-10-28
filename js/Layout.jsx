650e35ea454c9ae0d140b4961ace10785d78f5ae
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-10-28 01:21:09 -1000

    added the footer

--- js/Layout.jsx
+++ js/Layout.jsx
@@ -1,10 +1,14 @@
 const React = require('react')
 const HeaderNavbar = require('./HeaderNavbar')
+const Footer = require('./Footer')
 
 const Layout = (props) => (
   <div className='app-container'>
     <HeaderNavbar />
+    <br></br>
     {props.children}
+    <br></br>
+    <Footer className='footer'/>
   </div>
 )
 
