c7055c2947d8a547ea2c9a79d661866121e97796
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-10-28 01:21:21 -1000

    removed the header and the footer

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,7 +1,5 @@
 const React = require('react')
 const { Link } = require('react-router')
-const Header = require('./HeaderNavbar')
-const Footer = require('./Footer')
 const MyTitle = require('./MyTitle')
 const SongList = require('./SongList')
 
@@ -9,11 +7,9 @@ class MyFirstComponent extends React.Component {
   render () {
     return (
       <div>
-        <Header className='header' />
         <MyTitle title='Aloha this is kainoa' />
         <SongList />
         <Link to='/OtherRoute'>click to english</Link>
-        <Footer />
       </div>
     )
   }
