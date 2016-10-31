00553b862f69c49d50d8957948daab1189c403b5
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-10-30 20:52:12 -1000

    created get and post album routes

--- routes/album.js
+++ routes/album.js
@@ -1,11 +1,42 @@
 'use strict'
 
-const express = require('express')
-const router = express.Router()
-const bodyParser = require('body-parser')
-const db = require('./../models')
-const Album = db.Album
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const db = require('./../models');
+const Album = db.Album;
 
-router.use(bodyParser.json({ extended: false }))
+router.use(bodyParser.json({ extended: false }));
 
-module.exports = router
+router.get('/', function(req, res) {
+  Album.findAll()
+    .then (function(albums){
+      return res.json(albums);
+    });
+});
+
+router.post('/', function(req, res) {
+  Album.create({
+    title: req.body.title,
+    description: req.body.description,
+    ArtistId: req.body.ArtistId,
+    RecordLabelId: req.body.RecordLabelId,
+    visibility: false
+  })
+  .then(function(albums){
+    return res.json(albums)
+  })
+})
+
+router.get('/ofArtist/:id', function(req, res) {
+  Album.findAll({
+    where: {
+      ArtistId: encodeURI(req.params.id)
+    }
+  })
+  .then(function(albums){
+    return res.json(albums);
+  });
+});
+
+module.exports = router;
