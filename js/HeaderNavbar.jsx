fe7d8adf8153ee6f8a6e340c98e1aedb79af394f
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-11-01 12:30:45 -1000

    added styling

--- js/HeaderNavbar.jsx
+++ js/HeaderNavbar.jsx
@@ -1,21 +1,45 @@
 const React = require('react')
 const { Link } = require('react-router')
 
+const style = {
+  divContainer: {
+    backgroundColor: 'yellow'
+  },
+  KanakaMusicHeader: {
+    textDecoration: 'none',
+    fontSize: 50,
+    backgroundColor: 'pink',
+    width: '100%'
+  },
+  headerChildren: {
+    display: 'flex',
+    flexFlow: 'row no-wrap',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    justifyContent: 'space-around'
+  },
+  headerLinks: {
+    textDecoration: 'none',
+    color: 'black',
+    flex: '1 0 20px'
+  }
+}
+
 const HeaderNavbar = () => (
-  <div id='header-bar' className='HeaderNavbar'>
-    <Link to='/'>KanakaMusic</Link>
-    <h4>This is the header</h4>
-    <Link to='/addNewSong'> +Song </Link>
-    <p>Search</p>
-    <form>
-      <input type='text' />
-    </form>
-    <p>Songs</p>
-    <Link to='/Gigs'> Gigs </Link>
-    <br />
-    <Link to='/Connect'> Connect </Link>
-    <p>Chords</p>
-    <Link to='/Settings'> Settings </Link>
+  <div id='header-bar' className='HeaderNavbar' style={style.divContainer}>
+    <div>
+      <Link to='/' id='KanakaMusic-Header' style={style.KanakaMusicHeader}>KanakaMusic</Link>
+    </div>
+    <div style={style.headerChildren}>
+      <div><Link to='/addNewSong' style={style.headerLinks} > +Song </Link></div>
+      <div><Link to='/Gigs' style={style.headerLinks} > Gigs </Link></div>
+      <div><Link to='/Connect' style={style.headerLinks} > Connect </Link></div>
+      <div><p style={style.headerLinks} >Chords</p></div>
+      <form >
+        <input type='text' placeholder='Search'/>
+      </form>
+      <div><Link to='/Settings' style={style.headerLinks} > Settings </Link></div>
+    </div>
   </div>
 )
 
