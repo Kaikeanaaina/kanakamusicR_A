8f64c694260d59e91d3c119f208960f996bf4db5
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-11-01 12:30:53 -1000

    added styling

--- js/Footer.jsx
+++ js/Footer.jsx
@@ -1,18 +1,47 @@
 const React = require('react')
 const { Link } = require('react-router')
 
+const style = {
+  divContainer: {
+    backgroundColor: 'orange'
+  },
+  footerChildren: {
+    display: 'flex',
+    flexFlow: 'row no-wrap',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    justifyContent: 'space-around'
+  },
+  footerLinks: {
+    textDecoration: 'none',
+    color: 'black',
+    flex: '1 0 20px'
+  },
+  footerSocials: {
+    display: 'flex',
+    flexFlow: 'row no-wrap',
+    justifyContent: 'space-around'
+  },
+  footerSocialsLink: {
+    textDecoration: 'none',
+    color: 'black',
+    backgroundColor: 'yellow',
+    flex: '1 0 20px'
+  }
+}
+
 const Footer = () => (
-  <div id='footer' className='Footer'>
-    <h5> this is the footer</h5>
-    <p> social medias</p>
-    <p> email list</p>
-    <Link to='/about'> About </Link>
-    <br />
-    <Link to='/PrivacyPolicy'> PrivacyPolicy </Link>
-    <br />
-    <Link to='/TermsOfService'> TermsOfService </Link>
-    <br />
-    <Link to='/ContactInformation'> ContactInformation </Link>
+  <div id='footer' className='Footer' style={style.divContainer}>
+    <div style={style.footerSocials}>
+      <div><p style={style.footerSocialsLink}> social medias </p></div>
+      <div><p style={style.footerSocialsLink}> email list</p></div>
+    </div>
+    <div style={style.footerChildren}>
+      <div><Link to='/about' style={style.footerLinks}> About </Link></div>
+      <div><Link to='/PrivacyPolicy' style={style.footerLinks}> PrivacyPolicy </Link></div>
+      <div><Link to='/TermsOfService' style={style.footerLinks}> TermsOfService </Link></div>
+      <div><Link to='/ContactInformation' style={style.footerLinks}> ContactInformation </Link></div>
+    </div>
   </div>
 )
 
