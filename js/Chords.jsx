ecb4d0aed3f3a565ff54acaab8b86b4254b67c31
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-11-06 18:30:53 -1000

    added the chords file

---
+++ js/Chords.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+
+class Chords extends React.Component {
+  render () {
+    return (
+      <div>
+        <h2>Chords</h2>
+      </div>
+    )
+  }
+}
+
+module.exports = Chords
