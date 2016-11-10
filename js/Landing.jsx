a39dd9738639c636b16e7213ad2b67dd323503b5
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-11-10 12:55:30 -0800

    added a switch between song and artist list

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,15 +1,41 @@
 const React = require('react')
-const { Link } = require('react-router')
-const MyTitle = require('./MyTitle')
 const SongList = require('./SongList')
+const ArtistList = require('./ArtistList')
 
 class MyFirstComponent extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      SongList: true
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick (e) {
+    this.setState({
+      SongList: !this.state.SongList
+    })
+  }
   render () {
+    let SongListing = null
+    let ArtistListing = null
+    let ButtonSwitch = null
+    if (this.state.SongList) {
+      SongListing = <div><SongList /></div>
+      ArtistListing = null
+    } else {
+      ArtistListing = <div><ArtistList /></div>
+      SongListing = null
+    }
+    if (this.state.SongList) {
+      ButtonSwitch = <div className='buttons'><button onClick={this.handleClick} > Switch to Artists </button></div>
+    } else {
+      ButtonSwitch = <div className='buttons'><button onClick={this.handleClick} > Switch to Songs </button></div>
+    }
     return (
       <div>
-        <MyTitle title='Aloha this is kainoa' />
-        <SongList />
-        <Link to='/OtherRoute'>click to english</Link>
+        {ButtonSwitch}
+        {SongListing}
+        {ArtistListing}
       </div>
     )
   }
