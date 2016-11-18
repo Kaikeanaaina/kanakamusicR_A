c51c5bd80734c6f564bf4de4a7fab7bd47696b4b
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-11-17 17:31:52 -1000

    add edit album component

---
+++ js/EditAlbum.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+
+class EditAlbum extends React.Component {
+  render () {
+    return (
+      <div>
+        edit album here
+      </div>
+    )
+  }
+}
+
+module.exports = EditAlbum
