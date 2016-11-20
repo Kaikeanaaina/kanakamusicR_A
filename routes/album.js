f4cdd47b857c195e22708722da69b436ff8bf7a8
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-11-19 20:27:34 -1000

    add get and delete request of certain songs

--- routes/album.js
+++ routes/album.js
@@ -15,20 +15,7 @@ router.get('/', function (req, res) {
     })
 })
 
-router.post('/', function (req, res) {
-  Album.create({
-    title: req.body.title,
-    description: req.body.description,
-    ArtistId: req.body.ArtistId,
-    RecordLabelId: req.body.RecordLabelId,
-    visibility: false
-  })
-  .then(function (albums) {
-    return res.json(albums)
-  })
-})
-
-router.get('/ofArtist/:id', function (req, res) {
+router.get('/ByArtistId/:id', function (req, res) {
   Album.findAll({
     where: {
       ArtistId: encodeURI(req.params.id)
@@ -49,5 +36,48 @@ router.get('/:id', function (req, res) {
     return res.json(album)
   })
 })
+
+router.post('/', function (req, res) {
+  Album.create({
+    title: req.body.title,
+    description: req.body.description,
+    ArtistId: req.body.ArtistId,
+    RecordLabelId: req.body.RecordLabelId,
+    visibility: false
+  })
+  .then(function (albums) {
+    return res.json(albums)
+  })
+})
+
+
+router.put('/:id', function(req, res){
+  Album.update(
+  {
+    updatedAt : 'now()',
+    title : req.body.title,
+    description : req.body.description,
+    RecordLabelId : req.body.RecordLabelId,
+    visibility : req.body.visibility
+  }, {
+    where : {
+      id : req.params.id
+    }
+  })
+  .then(function(album){
+    return res.json(album);
+  });
+});
+
+router.delete('/:id', function(req, res){
+  Album.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(function(data){
+    return res.json(data);
+  });
+});
 
 module.exports = router
