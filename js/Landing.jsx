5882c976ae885717a427d274eb9f58a99ea4f4d1
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-11-21 18:40:36 -1000

    render condition if search has value

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,46 +1,15 @@
 const React = require('react')
-const SongList = require('./SongList')
-const ArtistList = require('./ArtistList')
-const AlbumList = require('./AlbumList')
+const SearchResults = require('./SearchResults')
+const ContentLists = require('./ContentLists')
 
 class MyFirstComponent extends React.Component {
   constructor () {
     super()
     this.state = {
-      SongList: true,
-      ArtistList: false,
-      AlbumList: false,
       SearchResult: false,
       searchInput: ''
     }
-    this.ShowSongList = this.ShowSongList.bind(this)
-    this.ShowArtistList = this.ShowArtistList.bind(this)
-    this.ShowAlbumList = this.ShowAlbumList.bind(this)
     this.searchInputEvent = this.searchInputEvent.bind(this)
-  }
-  ShowSongList (e) {
-    this.setState({
-      SongList: true,
-      ArtistList: false,
-      AlbumList: false,
-      SearchResult: false
-    })
-  }
-  ShowArtistList (e) {
-    this.setState({
-      SongList: false,
-      ArtistList: true,
-      AlbumList: false,
-      SearchResult: false
-    })
-  }
-  ShowAlbumList (e) {
-    this.setState({
-      SongList: false,
-      ArtistList: false,
-      AlbumList: true,
-      SearchResult: false
-    })
   }
   searchInputEvent (e) {
     this.setState({
@@ -49,34 +18,22 @@ class MyFirstComponent extends React.Component {
 
     if (!e.target.value) {
       this.setState({
-        SongList: true,
         SearchResult: false
       })
     } else {
       this.setState({
-        SongList: false,
         SearchResult: true
       })
     }
   }
   render () {
-    let Listing = null
     let ResultListing = null
 
-    if (this.state.SongList) {
-      Listing = <div><SongList /></div>
-    } else if (this.state.ArtistList) {
-      Listing = <div><ArtistList /></div>
-    } else {
-      Listing = <div><AlbumList /></div>
-    }
-
     if (this.state.SearchResult) {
-      ResultListing = (<div>hi</div>)
+      ResultListing = (<div><SearchResults searchInput={this.state.searchInput} /></div>)
     } else {
-      ResultListing = (<div><div className='buttons'><button onClick={this.ShowSongList} > Song is true </button><button onClick={this.ShowArtistList} > Artist is true </button><button onClick={this.ShowAlbumList} > Album is true </button></div>{Listing}</div>)
+      ResultListing = (<div><ContentLists /></div>)
     }
-
     return (
       <div>
         <div>
