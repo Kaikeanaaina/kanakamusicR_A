96b7b7e84c8cd75587f5a74ba13fbd92d3952df4
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-11-21 21:39:13 -1000

    placed all add new content in one component

---
+++ js/AddNewContent.jsx
@@ -0,0 +1,80 @@
+const React = require('react')
+const AddNewSong = require('./AddNewSong')
+const AddNewArtist = require('./AddNewArtist')
+const AddNewAlbum = require('./AddNewAlbum')
+const AddNewRecordLabel = require('./AddNewRecordLabel')
+
+class AddNewContent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      ShowAddSongForm: true,
+      ShowAddArtistForm: false,
+      ShowAddAlbumForm: false,
+      ShowAddRecordLabelForm: false
+    }
+    this.ShowAddSongForm = this.ShowAddSongForm.bind(this)
+    this.ShowAddArtistForm = this.ShowAddArtistForm.bind(this)
+    this.ShowAddAlbumForm = this.ShowAddAlbumForm.bind(this)
+    this.ShowAddRecordLabelForm = this.ShowAddRecordLabelForm.bind(this)
+  }
+  ShowAddSongForm (e) {
+    this.setState({
+      ShowAddSongForm: true,
+      ShowAddAlbumForm: false,
+      ShowAddArtistForm: false,
+      ShowAddRecordLabelForm: false
+    })
+  }
+  ShowAddAlbumForm (e) {
+    this.setState({
+      ShowAddSongForm: false,
+      ShowAddAlbumForm: true,
+      ShowAddArtistForm: false,
+      ShowAddRecordLabelForm: false
+    })
+  }
+  ShowAddArtistForm (e) {
+    this.setState({
+      ShowAddSongForm: false,
+      ShowAddAlbumForm: false,
+      ShowAddArtistForm: true,
+      ShowAddRecordLabelForm: false
+    })
+  }
+  ShowAddRecordLabelForm (e) {
+    this.setState({
+      ShowAddSongForm: false,
+      ShowAddAlbumForm: false,
+      ShowAddArtistForm: false,
+      ShowAddRecordLabelForm: true
+    })
+  }
+  render () {
+    let form
+    if (this.state.ShowAddSongForm) {
+      form = (<div><AddNewSong /></div>)
+    } else if (this.state.ShowAddArtistForm) {
+      form = (<div><AddNewArtist /></div>)
+    } else if (this.state.ShowAddAlbumForm) {
+      form = (<div><AddNewAlbum /></div>)
+    } else {
+      form = (<div><AddNewRecordLabel /></div>)
+    }
+    return (
+      <div>
+        <div>
+          <button onClick={this.ShowAddSongForm}> + Song </button>
+          <button onClick={this.ShowAddArtistForm}> + Artist </button>
+          <button onClick={this.ShowAddAlbumForm}> + Album </button>
+          <button onClick={this.ShowAddRecordLabelForm}> + Record Label </button>
+        </div>
+        <div>
+          {form}
+        </div>
+      </div>
+    )
+  }
+}
+
+module.exports = AddNewContent
