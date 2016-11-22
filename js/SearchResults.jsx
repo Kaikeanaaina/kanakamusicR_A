011e1ba0e1a3c97e1f9215502d6b8ebadf4b530e
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-11-21 19:12:31 -1000

    filter and map the artist dependent on the search

--- js/SearchResults.jsx
+++ js/SearchResults.jsx
@@ -16,6 +16,20 @@ const style = {
   SongText: {
     borderBottom: 'black solid 1px',
     color: 'grey'
+  },
+  albumListContainer: {
+
+  },
+  albumLinkContainer: {
+
+  },
+  albumLink: {
+    textDecoration: 'none',
+    fontSize: '24px'
+  },
+  AlbumText: {
+    borderBottom: 'black solid 1px',
+    color: 'grey'
   }
 }
 
@@ -54,18 +68,34 @@ class SearchResults extends React.Component {
         <h1>
           All Search Results
         </h1>
-        {this.state.songs
-          .filter((song) => `${song.title} ${song.description}`.toUpperCase().indexOf(this.props.searchInput.toUpperCase()) >= 0)
-          .map((song, i) => (
-            <div style={style.songLinkContainer} key={i} >
-              <Link key={i} to={`/song/${song.id}`} style={style.songLink} >
-                <div key={i} style={style.SongText}>
-                  {song.title}
-                </div>
-                <br />
-              </Link>
-            </div>
-        ))}
+        <div>
+          {this.state.songs
+            .filter((song) => `${song.title} ${song.description}`.toUpperCase().indexOf(this.props.searchInput.toUpperCase()) >= 0)
+            .map((song, i) => (
+              <div style={style.songLinkContainer} key={i} >
+                <Link key={i} to={`/song/${song.id}`} style={style.songLink} >
+                  <div key={i} style={style.SongText}>
+                    {song.title}
+                  </div>
+                  <br />
+                </Link>
+              </div>
+          ))}
+        </div>
+        <div>
+          {this.state.albums
+            .filter((album) => `${album.title} ${album.description}`.toUpperCase().indexOf(this.props.searchInput.toUpperCase()) >= 0)
+            .map((album, i) => (
+              <div style={style.albumLinkContainer} key={i} >
+                <Link key={i} to={`/album/${album.id}`} style={style.albumLink} >
+                  <div key={i} style={style.AlbumText} >
+                    {album.title}
+                  </div>
+                  <br />
+                </Link>
+              </div>
+          ))}
+        </div>
       </div>
     )
   }
