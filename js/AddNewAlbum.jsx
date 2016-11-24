2746515066aa5dda66f9922129e7f23eeb62b7f1
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-11-23 14:27:30 -1000

    removed links

--- js/AddNewAlbum.jsx
+++ js/AddNewAlbum.jsx
@@ -1,19 +1,14 @@
 const React = require('react')
-const { Link } = require('react-router')
 const axios = require('axios')
 
 class AddNewAlbum extends React.Component {
   constructor (props) {
     super(props)
-    this.recordLabelChange = this.recordLabelChange.bind(this)
-    this.artistChange = this.artistChange.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
     this.state = {
       recordLabels: [],
       type: '',
-      artists: [],
-      linkToNewArtist: false,
-      linkToNewRecordLabel: false
+      artists: []
     }
   }
   componentDidMount () {
@@ -29,30 +24,6 @@ class AddNewAlbum extends React.Component {
         artists: res.data
       })
     })
-  }
-  recordLabelChange (e) {
-    e.preventDefault()
-    if (e.target.value === 'giveAddNewRecordLabelLink') {
-      this.setState({
-        linkToNewRecordLabel: true
-      })
-    } else {
-      this.setState({
-        linkToNewRecordLabel: false
-      })
-    }
-  }
-  artistChange (e) {
-    e.preventDefault()
-    if (e.target.value === 'giveAddNewArtistLink') {
-      this.setState({
-        linkToNewArtist: true
-      })
-    } else {
-      this.setState({
-        linkToNewArtist: false
-      })
-    }
   }
   onSubmit (e) {
     e.preventDefault()
@@ -80,37 +51,27 @@ class AddNewAlbum extends React.Component {
     })
   }
   render () {
-    let AddNewArtistLink = null
-    if (this.state.linkToNewArtist) {
-      AddNewArtistLink = <div><br /><Link to='/AddNewArtist'>Add New Artist</Link><br /></div>
-    }
-    let AddNewRecordLabelLink = null
-    if (this.state.linkToNewRecordLabel) {
-      AddNewRecordLabelLink = <div><br /><Link to='/AddNewRecordLabel'>Add New Record Label</Link><br /></div>
-    }
     return (
       <div id='AddNewAlbum'>
         <h3>Add New Album</h3>
         <form onSubmit={this.onSubmit}>
           <input type='text' ref='title' placeholder='title' />
           <br />
-          <select onChange={this.artistChange} ref='artist'>
+          <select ref='artist'>
             <option value='' >artist here</option>
             <option value='giveAddNewArtistLink'>AddNewArtist</option>
             {this.state.artists.map((artist, index) => (
               <option key={index} value={artist.id} > {artist.name} </option>
             ))}
           </select>
-          {AddNewArtistLink}
           <br />
-          <select onChange={this.recordLabelChange} ref='recordLabel'>
+          <select ref='recordLabel'>
             <option value='' >record label here</option>
             <option value='giveAddNewRecordLabelLink'>AddNewRecordLabel</option>
             {this.state.recordLabels.map((recordlabel, index) => (
               <option key={index} value={recordlabel.id} > {recordlabel.name} </option>
             ))}
           </select>
-          {AddNewRecordLabelLink}
           <br />
           <textarea type='text' ref='description' placeholder='description' />
           <br />
