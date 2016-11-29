7b7d8f748cb54de20955d76644e9ffff7265a663
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-11-28 23:51:27 -1000

    record label button with list of record labels

--- js/MainContent/ContentLists.jsx
+++ js/MainContent/ContentLists.jsx
@@ -2,6 +2,7 @@ const React = require('react')
 const SongList = require('./Song/SongList')
 const ArtistList = require('./Artist/ArtistList')
 const AlbumList = require('./Album/AlbumList')
+const RecordLabelList = require('./RecordLabel/RecordLabelList')
 
 class ContentLists extends React.Component {
   constructor () {
@@ -9,31 +10,44 @@ class ContentLists extends React.Component {
     this.state = {
       SongList: true,
       ArtistList: false,
-      AlbumList: false
+      AlbumList: false,
+      RecordLabelList: false
     }
     this.ShowSongList = this.ShowSongList.bind(this)
     this.ShowArtistList = this.ShowArtistList.bind(this)
     this.ShowAlbumList = this.ShowAlbumList.bind(this)
+    this.ShowRecordLabelList = this.ShowRecordLabelList.bind(this)
   }
   ShowSongList (e) {
     this.setState({
       SongList: true,
       ArtistList: false,
-      AlbumList: false
+      AlbumList: false,
+      RecordLabel: false
     })
   }
   ShowArtistList (e) {
     this.setState({
       SongList: false,
       ArtistList: true,
-      AlbumList: false
+      AlbumList: false,
+      RecordLabel: false
     })
   }
   ShowAlbumList (e) {
     this.setState({
       SongList: false,
       ArtistList: false,
-      AlbumList: true
+      AlbumList: true,
+      RecordLabel: false
+    })
+  }
+  ShowRecordLabelList (e) {
+    this.setState({
+      SongList: false,
+      ArtistList: false,
+      AlbumList: false,
+      RecordLabelList: true
     })
   }
   render () {
@@ -43,8 +57,10 @@ class ContentLists extends React.Component {
       Listing = <div><SongList /></div>
     } else if (this.state.ArtistList) {
       Listing = <div><ArtistList /></div>
-    } else {
+    } else if (this.state.AlbumList) {
       Listing = <div><AlbumList /></div>
+    } else {
+      Listing = <div><RecordLabelList /></div>
     }
     return (
       <div>
@@ -52,6 +68,7 @@ class ContentLists extends React.Component {
           <button onClick={this.ShowSongList} > Song is true </button>
           <button onClick={this.ShowArtistList} > Artist is true </button>
           <button onClick={this.ShowAlbumList} > Album is true </button>
+          <button onClick={this.ShowRecordLabelList} > RecordLabel is true </button>
         </div>
         {Listing}
       </div>
