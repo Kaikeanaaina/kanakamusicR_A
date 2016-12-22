21216262d40879c53c662c9bbcce130149f975b8
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-12-21 21:42:59 -1000

    test

--- test/SongList.spec.js
+++ test/SongList.spec.js
@@ -7,8 +7,53 @@ const { mount, shallow } = require('enzyme')
 const SongList = require('../js/MainContent/Song/SongList')
 
 describe('<SongList />', () => {
-  it('should have an initial songs state', () => {
-    const wrapper = mount(<SongList />)
-    expect(wrapper.state().songs.length).to.equal(0)
+
+  describe('state', () => {
+
+    it('should have an initial songs state', () => {
+      const wrapper = mount(<SongList />)
+      expect(wrapper.state().songs.length).to.equal(0)
+    })
+
+    it('should all have all div that are arrays', () => {
+      const wrapper = mount(<SongList />)
+      expect(wrapper.state().songs4DivArray1).to.be.defined
+      expect(wrapper.state().songs4DivArray1).to.be.an('array')
+      expect(wrapper.state().songs4DivArray1.length).to.equal(0)
+
+      expect(wrapper.state().songs4DivArray2).to.be.defined
+      expect(wrapper.state().songs4DivArray2).to.be.an('array')
+      expect(wrapper.state().songs4DivArray2.length).to.equal(0)
+
+      expect(wrapper.state().songs4DivArray3).to.be.defined
+      expect(wrapper.state().songs4DivArray3).to.be.an('array')
+      expect(wrapper.state().songs4DivArray3.length).to.equal(0)
+
+      expect(wrapper.state().songs4DivArray4).to.be.defined
+      expect(wrapper.state().songs4DivArray4).to.be.an('array')
+      expect(wrapper.state().songs4DivArray4.length).to.equal(0)
+
+      expect(wrapper.state().songs3DivArray1).to.be.defined
+      expect(wrapper.state().songs3DivArray1).to.be.an('array')
+      expect(wrapper.state().songs3DivArray1.length).to.equal(0)
+
+      expect(wrapper.state().songs3DivArray2).to.be.defined
+      expect(wrapper.state().songs3DivArray2).to.be.an('array')
+      expect(wrapper.state().songs3DivArray2.length).to.equal(0)
+
+      expect(wrapper.state().songs3DivArray3).to.be.defined
+      expect(wrapper.state().songs3DivArray3).to.be.an('array')
+      expect(wrapper.state().songs3DivArray3.length).to.equal(0)
+
+      expect(wrapper.state().songs2DivArray1).to.be.defined
+      expect(wrapper.state().songs2DivArray1).to.be.an('array')
+      expect(wrapper.state().songs2DivArray1.length).to.equal(0)
+
+      expect(wrapper.state().songs2DivArray2).to.be.defined
+      expect(wrapper.state().songs2DivArray2).to.be.an('array')
+      expect(wrapper.state().songs2DivArray2.length).to.equal(0)
+    })
+
   })
+
 })
