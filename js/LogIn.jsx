58444d74e4cb2393f128b2d1c0c895466d835f09
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-12-23 12:19:12 -1000

    input

--- js/LogIn.jsx
+++ js/LogIn.jsx
@@ -1,19 +1,39 @@
 const React = require('react')
+const axios = require('axios')
+const { domain } = require('./MainContent/Domain')
 
 class LogIn extends React.Component {
   constructor () {
     super()
-    this.state = {
-      blank: ''
+    this.logIn = this.logIn.bind(this)
+  }
+  logIn (e) {
+    e.preventDefault()
+
+    let object = {
+      email: this.refs.email,
+      password: this.refs.password
     }
+
+    axios.post(`${domain}/users/login`, object)
+    .then((res) => {
+
+    })
+    .catch((error) => {
+      console.log('axios error', error)
+    })
   }
   render () {
     return (
       <div>
-        <h1>log in</h1>
-        <input type='text' />
-        <input type='password' />
-        <button>submit</button>
+        <h3>Log In </h3>
+        <form onSubmit={this.logIn}>
+          <label>
+            <input type='text' ref='email' placeholder='email' />
+            <input type='password' ref='password' placeholder='password' />
+          </label>
+          <button>Log In</button>
+        </form>
       </div>
     )
   }
