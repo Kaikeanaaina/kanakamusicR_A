e7c49d9826b26aebeedcd590f8b7d1a46b92f06f
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-12-28 18:23:22 -1000

    gigs

--- js/Gigs.jsx
+++ js/Gigs.jsx
@@ -6,6 +6,27 @@ class Gigs extends React.Component {
     return (
       <div>
         <MyTitle title='Gigs' />
+
+        <div style={{backgroundColor: 'rgba(0,0,255, 0.4)'}}>
+          <h2>filters</h2>
+          <input placeholder='search' />
+          <h3>search bar</h3>
+          <h3>date bar</h3>
+          <h3>calendar</h3>
+        </div>
+
+        <div style={{backgroundColor: 'rgba(0,255,0,0.4)'}}>
+          <h3>Event Name</h3>
+          <h3>Date</h3>
+          <h3>Times</h3>
+          <h3>Artists</h3>
+          <h3>Venue</h3>
+          <h3>Age</h3>
+          <h3>Ticket Price</h3>
+          <h3>Promoters</h3>
+          <h3>contact</h3>
+        </div>
+
       </div>
     )
   }
