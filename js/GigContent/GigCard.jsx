ac4fa8f4d52bf515fac88fb87a8905d7ca01e23e
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-01-05 12:43:01 -1000

    organize the this.props.gig to their respective label

--- js/GigContent/GigCard.jsx
+++ js/GigContent/GigCard.jsx
@@ -32,21 +32,73 @@ class GigCard extends React.Component {
     })
   }
   render () {
+    let price
+    if (this.props.gig.price === 0) {
+      price = (<p>Free</p>)
+    } else {
+      price = (<p>${this.props.gig.price}</p>)
+    }
+    let age
+    if (this.props.gig.age === 0) {
+      age = (<p>All Ages</p>)
+    } else {
+      age = (<p>{this.props.gig.age}</p>)
+    }
+    let time
+    if (this.props.gig.startHour < 12) {
+      time = (<p>{this.props.gig.startHour}:{this.props.gig.startMinute} am</p>)
+    } else {
+      time = (<p>{this.props.gig.startHour - 12}:{this.props.gig.startMinute} pm</p>)
+    }
     return (
       <div>
         <h1> Gig Card </h1>
 
         <div style={{backgroundColor: 'rgba(0,255,0,0.4)'}}>
-          <h3>{this.props.gig.name}</h3>
-          <h3>{this.state.Artist.name}</h3>
-          <h3>{this.props.gig.Month}</h3>
-          <h3>{this.props.gig.Day}</h3>
-          <h3>{this.state.Venue.name}</h3>
-          <h3>{this.props.gig.age}</h3>
-          <h3>{this.props.gig.price}</h3>
-          <h3>{this.props.gig.promoter}</h3>
-          <h3>{this.props.gig.contact}</h3>
-          <h3>{this.props.gig.description}</h3>
+          <label>
+            <strong>Name:</strong>
+            <p>{this.props.gig.name}</p>
+          </label>
+          <label>
+            <strong>Artists:</strong>
+            <p>{this.state.Artist.name}</p>
+          </label>
+
+          <div>
+            <label>
+              <strong>Date:</strong>
+              <p>{this.props.gig.Month}/{this.props.gig.Day}/{this.props.gig.Year}</p>
+            </label>
+            <label>
+              <strong>Time:</strong>
+              {time}
+            </label>
+          </div>
+
+          <label>
+            <strong>Venue:</strong>
+            <p>{this.state.Venue.name}</p>
+          </label>
+          <label>
+            <strong>Age:</strong>
+            {age}
+          </label>
+          <label>
+            <strong>Price:</strong>
+            {price}
+          </label>
+          <label>
+            <strong>Promoters:</strong>
+            <p>{this.props.gig.promoter}</p>
+          </label>
+          <label>
+            <strong>Contact:</strong>
+            <p>{this.props.gig.contact}</p>
+          </label>
+          <label>
+            <strong>Description:</strong>
+            <p>{this.props.gig.description}</p>
+          </label>
         </div>
 
       </div>
