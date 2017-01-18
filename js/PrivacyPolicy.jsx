5c1f4d8b3838d5d5fa9f014a502744b5265051bf
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-01-18 11:20:38 -1000

    ajax call go to privacyPolicies

--- js/PrivacyPolicy.jsx
+++ js/PrivacyPolicy.jsx
@@ -1,11 +1,38 @@
 const React = require('react')
 const MyTitle = require('./MyTitle')
+const { Link } = require('react-router')
+const axios = require('axios')
+const { domain } = require('./Domain')
+
 
 class PrivacyPolicy extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      privacyPolicy: ''
+    }
+  }
+  componentDidMount () {
+    axios.get(`${domain}/privacyPolicies/1`)
+    .then((res) => {
+      this.setState({
+        privacyPolicy: res.data.privacyPolicy
+      })
+    })
+    .catch((err) => {
+      console.log('axios error', err)
+    })
+  }
   render () {
     return (
       <div>
         <MyTitle title='PrivacyPolicy' />
+        <button>
+          <Link to='/EditPrivacyPolicy'> Edit Privacy Policy </Link>
+        </button>
+
+        <p>{this.state.privacyPolicy}</p>
+
       </div>
     )
   }
