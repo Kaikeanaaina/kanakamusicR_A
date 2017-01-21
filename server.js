516ab31712b009fbf503c102e4f6e31f1d4d995f
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-01-20 23:22:57 -1000

    middleware for terms of service

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use('/gigs', require('./routes/gig.js'))
 app.use('/privacyPolicies', require('./routes/privacyPolicy.js'))
 app.use('/resources', require('./routes/resource.js'))
 app.use('/credits', require('./routes/credit.js'))
+app.use('/termsOfServices', require('./routes/termsOfService.js'))
 
 app.use((req, res) => {
   match({ routes: Routes(), location: req.url }, (error, redirectLocation, renderProps) => {
