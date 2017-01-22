098a0c67c4286c6f268b8f0e7b9ef77f17e905db
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-01-22 00:53:42 -1000

    middle ware for contact information

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use('/resources', require('./routes/resource.js'))
 app.use('/credits', require('./routes/credit.js'))
 app.use('/termsOfServices', require('./routes/termsOfService.js'))
 app.use('/about', require('./routes/about.js'))
+app.use('/contactInformation', require('./routes/contactInformation.js'))
 
 app.use((req, res) => {
   match({ routes: Routes(), location: req.url }, (error, redirectLocation, renderProps) => {
