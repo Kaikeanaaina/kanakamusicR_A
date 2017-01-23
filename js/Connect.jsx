653aeb4e728764cb9f34ce055797cd56f90b63fa
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-01-23 10:39:01 -1000

    added artist list

--- js/Connect.jsx
+++ js/Connect.jsx
@@ -1,11 +1,13 @@
 const React = require('react')
 const MyTitle = require('./MyTitle')
+const ArtistList = require('./MainContent/Artist/ArtistList')
 
 class Connect extends React.Component {
   render () {
     return (
       <div>
         <MyTitle title='Connect' />
+        <ArtistList type='connect' />
       </div>
     )
   }
