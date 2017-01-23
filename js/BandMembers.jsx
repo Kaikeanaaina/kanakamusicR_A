53dab4d9b80f222cc5e787f68cc28a23b8ae5307
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-01-23 10:31:53 -1000

    to show the artist within the band

---
+++ js/BandMembers.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+
+class BandMembers extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1> Band Member </h1>
+      </div>
+    )
+  }
+}
+
+module.exports = BandMembers
