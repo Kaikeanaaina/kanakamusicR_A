44a9a0411a57ef401a625319a481c338214e8f70
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-01-23 11:15:40 -1000

    ajax request of all bandmembers in the group

--- js/BandMembers.jsx
+++ js/BandMembers.jsx
@@ -1,6 +1,25 @@
 const React = require('react')
+const axios = require('axios')
+const { domain } = require('./Domain')
 
 class BandMembers extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      bandMembers: []
+    }
+  }
+  componentDidMount () {
+    axios.get(`${domain}/bandMembers`)
+    .then((res) => {
+      this.setState({
+        bandMembers: res.data
+      })
+    })
+    .catch((err) => {
+      console.log('axios error', err)
+    })
+  }
   render () {
     return (
       <div>
