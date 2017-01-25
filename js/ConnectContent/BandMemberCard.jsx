68d1e2ea0633689e0ab4d969edc92d2e9f95cf76
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-01-25 10:26:11 -1000

    moved the props ArtistId

--- js/ConnectContent/BandMemberCard.jsx
+++ js/ConnectContent/BandMemberCard.jsx
@@ -1,21 +1,30 @@
 const React = require('react')
 
+const style = {
+  backgroundColor: 'rgba(100,100,100,0.2)',
+  padding: '5px',
+  borderRadius: '5px',
+  margin: '5px'
+}
+
 const BandMemberCard = (props) => (
-  <div>
+  <div style={style}>
     <h1> {props.bandMember.name} </h1>
+    <p> {props.bandMember.ArtistId} </p>
     <p> {props.bandMember.instrument} </p>
     <p> {props.bandMember.howYouStartedToPlayMusic} </p>
     <p> {props.bandMember.whatMotivatesYouToPlayMusic} </p>
     <p> {props.bandMember.whoDoYouLookUpTo} </p>
     <p> {props.bandMember.additionComments} </p>
-    <p> {props.bandMember.ArtistId} </p>
   </div>
 )
 
-const { object } = React.PropTypes
+const { object, func } = React.PropTypes
 
 BandMemberCard.propTypes = {
-  bandMember: object
+  bandMember: object,
+  openEditModal: func,
+  openDeleteModal: func
 }
 
 module.exports = BandMemberCard
