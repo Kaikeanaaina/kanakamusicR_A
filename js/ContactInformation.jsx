d27c3f15338e007385b3f0eedb5c5c1ad3b6ec3e
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-01-30 20:31:04 -1000

    delete and edit functions

--- js/ContactInformation.jsx
+++ js/ContactInformation.jsx
@@ -19,15 +19,30 @@ class ContactInformation extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      Contact: {},
       contacts: [],
       addContactInformationModalIsOpen: false,
       editContactInformationModalIsOpen: false,
-      deleteContactInformationModalIsOpen: false
+      deleteContactInformationModalIsOpen: false,
+      name: '',
+      title: '',
+      email: '',
+      phoneNumber: '',
+      faxNumber: ''
     }
     this.openAddContactModal = this.openAddContactModal.bind(this)
     this.afterOpenModal = this.afterOpenModal.bind(this)
     this.closeModal = this.closeModal.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
+    this.openDeleteModal = this.openDeleteModal.bind(this)
+    this.openEditModal = this.openEditModal.bind(this)
+    this.changingName = this.changingName.bind(this)
+    this.changingTitle = this.changingTitle.bind(this)
+    this.changingEmail = this.changingEmail.bind(this)
+    this.changingPhoneNumber = this.changingPhoneNumber.bind(this)
+    this.changingFaxNumber = this.changingFaxNumber.bind(this)
+    this.editContact = this.editContact.bind(this)
+    this.deleteContact = this.deleteContact.bind(this)
   }
   componentDidMount () {
     axios.get(`${domain}/contactInformation/`)
@@ -51,7 +66,9 @@ class ContactInformation extends React.Component {
   }
   closeModal () {
     this.setState({
-      addContactInformationModalIsOpen: false
+      addContactInformationModalIsOpen: false,
+      deleteContactInformationModalIsOpen: false,
+      editContactInformationModalIsOpen: false
     })
   }
   onSubmit () {
@@ -92,6 +109,124 @@ class ContactInformation extends React.Component {
       })
       .catch((err) => {
         console.log('axios error', err)
+      })
+    })
+    .catch((err) => {
+      console.log('axios error', err)
+    })
+  }
+  openDeleteModal (e) {
+    axios.get(`${domain}/contactInformation/${e.target.value}`)
+    .then((res) => {
+      this.setState({
+        deleteContactInformationModalIsOpen: true,
+        Contact: res.data,
+        name: res.data.name,
+        title: res.data.title,
+        email: res.data.email,
+        phoneNumber: res.data.phoneNumber,
+        faxNumber: res.data.faxNumber
+      })
+    })
+  }
+  openEditModal (e) {
+    axios.get(`${domain}/contactInformation/${e.target.value}`)
+    .then((res) => {
+      this.setState({
+        editContactInformationModalIsOpen: true,
+        Contact: res.data,
+        name: res.data.name,
+        title: res.data.title,
+        email: res.data.email,
+        phoneNumber: res.data.phoneNumber,
+        faxNumber: res.data.faxNumber
+      })
+    })
+  }
+  changingName (e) {
+    this.setState({
+      name: e.target.value
+    })
+  }
+  changingTitle (e) {
+    this.setState({
+      title: e.target.value
+    })
+  }
+  changingEmail (e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+  changingPhoneNumber (e) {
+    this.setState({
+      phoneNumber: e.target.value
+    })
+  }
+  changingFaxNumber (e) {
+    this.setState({
+      faxNumber: e.target.value
+    })
+  }
+  editContact () {
+    let object = {}
+
+    if (!this.refs.name.value) {
+      object.name = this.state.name
+    } else {
+      object.name = this.refs.name.value
+    }
+
+    if (!this.refs.title.value) {
+      object.title = this.state.title
+    } else {
+      object.title = this.refs.title.value
+    }
+
+    if (!this.refs.email.value) {
+      object.email = this.state.email
+    } else {
+      object.email = this.refs.email.value
+    }
+
+    if (!this.refs.phoneNumber.value) {
+      object.phoneNumber = this.state.phoneNumber
+    } else {
+      object.phoneNumber = this.refs.phoneNumber.value
+    }
+
+    if (!this.refs.faxNumber.value) {
+      object.faxNumber = this.state.faxNumber
+    } else {
+      object.faxNumber = this.refs.faxNumber.value
+    }
+
+    axios.put(`${domain}/contactInformation/${this.state.Contact.id}`, object)
+    .then((res) => {
+      axios.get(`${domain}/contactInformation/`)
+      .then((res) => {
+        this.setState({
+          contacts: res.data,
+          editContactInformationModalIsOpen: false
+        })
+      })
+      .catch((err) => {
+        console.log('axios error', err)
+      })
+    })
+    .catch((err) => {
+      console.log('axios error', err)
+    })
+  }
+  deleteContact () {
+    axios.delete(`${domain}/contactInformation/${this.state.Contact.id}`)
+    .then((res) => {
+      axios.get(`${domain}/contactInformation/`)
+      .then((res) => {
+        this.setState({
+          contacts: res.data,
+          deleteContactInformationModalIsOpen: false
+        })
       })
     })
     .catch((err) => {
@@ -149,7 +284,65 @@ class ContactInformation extends React.Component {
             <button onClick={this.closeModal}>cancel</button>
             <button onClick={this.onSubmit}>submit</button>
           </Modal>
+        </div>
 
+        <div>
+          <Modal
+            isOpen={this.state.editContactInformationModalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel='editContact' >
+
+            <h4 ref='subtitle'> Edit Contact </h4>
+            <label>
+              Name:
+              <input type='text' value={this.state.name} ref='name' onChange={this.changingName} />
+            </label>
+            <br />
+            <label>
+              Title:
+              <input type='text' value={this.state.title} ref='title' onChange={this.changingTitle} />
+            </label>
+            <br />
+            <label>
+              Email:
+              <input type='text' value={this.state.email} ref='email' onChange={this.changingEmail} />
+            </label>
+            <br />
+            <label>
+              phoneNumber:
+              <input type='text' value={this.state.phoneNumber} ref='phoneNumber' onChange={this.changingPhoneNumber} />
+            </label>
+            <br />
+            <label>
+              faxNumber:
+              <input type='text' value={this.state.faxNumber} ref='faxNumber' onChange={this.changingFaxNumber} />
+            </label>
+            <br />
+            <button onClick={this.closeModal}>cancel</button>
+            <button onClick={this.editContact}>submit</button>
+
+          </Modal>
+        </div>
+
+        <div>
+          <Modal
+            isOpen={this.state.deleteContactInformationModalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel='deleteContact' >
+
+            <h4 ref='subtitle'> Are you sure you want to delete this contact? </h4>
+            <p>{this.state.Contact.name}</p>
+            <p>{this.state.Contact.title}</p>
+            <p>{this.state.Contact.email}</p>
+            <p>{this.state.Contact.phoneNumber}</p>
+            <p>{this.state.Contact.faxNumber}</p>
+            <button onClick={this.closeModal}>cancel</button>
+            <button onClick={this.deleteContact}>delete</button>
+          </Modal>
         </div>
 
       </div>
