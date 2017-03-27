daa29fb4302764f84cf6398c763a0eb000f8bcbb
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-03-27 13:45:59 -1000

    removed password

--- models/Artist.js
+++ models/Artist.js
@@ -4,10 +4,6 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING(255),
       allowNull: false
     },
-    password: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
     description: {
       type: DataTypes.STRING(255),
       allowNull: true
