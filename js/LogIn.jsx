c2d11810781fbbc79e87c022c0fe6f6836b3ec0d
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-09-06 20:50:42 -1000

    componentdidmount

--- js/LogIn.jsx
+++ js/LogIn.jsx
@@ -1,28 +1,46 @@
 const React = require('react')
-// const axios = require('axios')
-// const { domain } = require('./Domain')
+const axios = require('axios')
+const { domain } = require('./Domain')
 
 class LogIn extends React.Component {
   constructor () {
     super()
     this.logIn = this.logIn.bind(this)
   }
+  componentDidMount () {
+    console.log('1111111')
+    axios.get(`${domain}/users`)
+    .then((res) => {
+      console.log('33333333')
+      console.log('success')
+    })
+    .catch((error) => {
+      console.log('axios error', error)
+    })
+  }
   logIn (e) {
     e.preventDefault()
 
-    let object = {
-      email: this.refs.email,
-      password: this.refs.password
-    }
+    // let object = {
+    //   email: this.refs.email,
+    //   password: this.refs.password
+    // }
 
-    console.log(object.email.value, object.password.value)
-    // axios.post(`${domain}/users/login`, object)
+    // console.log(object.email.value, object.password.value)
+    // // axios.post(`${domain}/users/login`, object)
+    // // .then((res) => {
+    // axios.get(`/users/`)
     // .then((res) => {
-
+    //   console.log('success')
     // })
     // .catch((error) => {
     //   console.log('axios error', error)
     // })
+    // // })
+    // // .catch((error) => {
+    // //   console.log('axios error', error)
+    // // })
+    console.log('123123123132', this.refs.email, this.refs.password)
   }
   render () {
     return (
