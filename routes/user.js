c375d328cd52cfbce6970573e545cb14342cb921
KaiKeanaaina <kaikeanaaina@gmail.com>
2017-09-11 12:35:04 -1000

    creating a validation for posting a new user

--- routes/user.js
+++ routes/user.js
@@ -29,9 +29,28 @@ const exists = (req) => {
   }
 }
 
-router.get('/', function (req, res) {
-  console.log('22222222222')
+function registerValidation(req, res, next) {
+  if(req.body.email) {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+    .then((user) => {
+      if (user) {
+        return res.json({error:'email already exists'})
+      }
+      next()
+    })
+    .catch((error) => {
+      return res.json({error:'error'})
+    })
+  } else {
+    return res.json({error:'there is no email'})
+  }
+}
 
+router.get('/', function (req, res) {
   User.findAll({
     order: 'email'
   })
@@ -62,11 +81,12 @@ router.get('/:id', function (req, res) {
   // }
 })
 
-router.post('/', function (req, res) {
-  var urlname = (encodeURI(req.body.name)).toString()
-    User.create({
-    email: req.body.email,
-    password: req.body.password
+router.post('/', registerValidation, function (req, res) {
+  var stringEmail = (encodeURI(req.body.email)).toString()
+  User.create({
+    email: stringEmail,
+    password: req.body.password,
+    type: 'user'
   })
   .then(function (data) {
     return res.json(data)
@@ -74,6 +94,30 @@ router.post('/', function (req, res) {
   .catch(function (err) {
     return res.json({ error: err})
   })
+})
+
+router.post('/registrationValidation', function(req, res) {
+  var stringEmail = (encodeURI(req.body.email)).toString()
+
+  if(req.body.email) {
+    User.findOne({
+      where: {
+        email: stringEmail
+      }
+    })
+    .then((user) => {
+      if (user) {
+        return res.json({error:'email already exists'})
+      }
+    })
+    .catch((error) => {
+      return res.json({error:'error'})
+    })
+  } else if (req.body.email === null || req.body.email === undefined || req.body.email.length === 0) {
+    return res.json({error: 'need to input an email'})
+  } else {
+    return true
+  }
 })
 
 router.put('/:id', function (req, res) {
@@ -99,7 +143,6 @@ router.put('/:id', function (req, res) {
     res.json({succes: false})
   }
 })
-
 
 router.delete('/:id', function (req, res) {
   if (exists) {
