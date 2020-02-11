9db8ec95c8420d567e08392827a149ba6a9db0e3
KainoaKeanaaina <kaikeanaaina@gmail.com>
2020-02-11 09:40:07 -1000

    syntax fixes

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,50 +1,52 @@
-const React = require('react')
-const SearchResults = require('./MainContent/SearchResults')
-const ContentLists = require('./MainContent/ContentLists')
+// not being used
 
-class MyFirstComponent extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      SearchResult: false,
-      searchInput: ''
-    }
-    this.searchInputEvent = this.searchInputEvent.bind(this)
-  }
-  searchInputEvent (e) {
-    this.setState({
-      searchInput: e.target.value
-    })
+// const React = require('react')
+// const SearchResults = require('./MainContent/SearchResults')
+// const ContentLists = require('./MainContent/ContentLists')
 
-    if (!e.target.value) {
-      this.setState({
-        SearchResult: false
-      })
-    } else {
-      this.setState({
-        SearchResult: true
-      })
-    }
-  }
-  render () {
-    let ResultListing = null
+// class MyFirstComponent extends React.Component {
+//   constructor () {
+//     super()
+//     this.state = {
+//       SearchResult: false,
+//       searchInput: ''
+//     }
+//     this.searchInputEvent = this.searchInputEvent.bind(this)
+//   }
+//   searchInputEvent (e) {
+//     this.setState({
+//       searchInput: e.target.value
+//     })
 
-    if (this.state.SearchResult) {
-      ResultListing = (<div><SearchResults searchInput={this.state.searchInput} /></div>)
-    } else {
-      ResultListing = (<div><ContentLists /></div>)
-    }
-    return (
-      <div>
-        <div>
-          <form >
-            <input onChange={this.searchInputEvent} type='text' ref='search' placeholder='Search' />
-          </form>
-        </div>
-        {ResultListing}
-      </div>
-    )
-  }
-}
+//     if (!e.target.value) {
+//       this.setState({
+//         SearchResult: false
+//       })
+//     } else {
+//       this.setState({
+//         SearchResult: true
+//       })
+//     }
+//   }
+//   render () {
+//     let ResultListing = null
 
-module.exports = MyFirstComponent
+//     if (this.state.SearchResult) {
+//       ResultListing = (<div><SearchResults searchInput={this.state.searchInput} /></div>)
+//     } else {
+//       ResultListing = (<div><ContentLists /></div>)
+//     }
+//     return (
+//       <div>
+//         <div>
+//           <form >
+//             <input onChange={this.searchInputEvent} type='text' ref='search' placeholder='Search' />
+//           </form>
+//         </div>
+//         {ResultListing}
+//       </div>
+//     )
+//   }
+// }
+
+// module.exports = MyFirstComponent
